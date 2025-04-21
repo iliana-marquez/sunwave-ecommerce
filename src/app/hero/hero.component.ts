894ccd6ccf5ca3a992d249f46327e18ef82df183
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-04-21 16:27:46 +0200

    Set height resize logic to dinamically calculate the height of the hero section in the homepage

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    this.adjustHeroHeight();
+  }
+
+  @HostListener ('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.adjustHeroHeight();
+  }
+
+  adjustHeroHeight(): void {
+    const footer = document.querySelector('footer');
+    const navbar = document.querySelector('.navbar')
+    const hero = document.getElementById('hero');
+
+    if (footer && hero && navbar) {
+      const footerHeight = footer.getBoundingClientRect().height;
+      const navbarHeight = navbar?.getBoundingClientRect().height;
+      hero.style.minHeight = `calc(100vh - (${footerHeight}px + ${navbarHeight}px) )`;
+    }
+  }
 }
